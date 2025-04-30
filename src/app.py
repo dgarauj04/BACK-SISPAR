2c1e427a8838e62e4f7bcdd51789a94663c6dd5a
samuel-prata <samuel.silverio001@hotmail.com>
2025-04-30 10:30:16 -0300

    add: Aula 30/04 - Criando model de reembolsos

--- src/app.py
+++ src/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 from src.controller.colaborador_controller import bp_colaborador
 from src.model import db
 from config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins='*') # <---- Ative o CORS para todas as rotas da API
     app.register_blueprint(bp_colaborador)
     
     app.config.from_object(Config) # Trouxemos a configuração do ambiente de desenvolvimento
---
+++ src/controller/reembolso_controller.py
@@ -0,0 +1,5 @@
+
+
+# db.session.bulk_save_objects(Lista dos json)
+
+# id = 298
